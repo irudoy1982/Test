c7f1796cd44b0d1dcb8c8426a4d884bc5420ab80
irudoy1982 <irudoy1982@gmail.com>
2026-04-06 13:46:19 +0500

    Update audit_app.py

--- audit_app.py
+++ audit_app.py
@@ -53,11 +53,11 @@ col_h1, col_h2 = st.columns(2)
 
 with col_h1:
     client_info['Город'] = st.text_input("Город*")
-    client_info['Наименование компании'] = st.text_input("Наименование компании*")
-
     ndustry = st.selectbox("Сфера деятельности компании", 
                        ["Финтех / Банки", "Ритейл / E-commerce", "Производство", "IT / Разработка", "Госсектор", "Другое"])
-    
+    client_info['Наименование компании'] = st.text_input("Наименование компании*")
+
+ 
     site_input = st.text_input("Сайт компании*", key="site_field", placeholder="example.kz")
     client_info['Сайт компании'] = site_input
 
