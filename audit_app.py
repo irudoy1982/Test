6d90bc1cf631085598cbbe72ba49575c4ddb0a8b
irudoy1982 <irudoy1982@gmail.com>
2026-04-27 16:38:02 +0500

    Update audit_app.py

--- audit_app.py
+++ audit_app.py
@@ -256,6 +256,28 @@ if st.checkbox("Резервное копирование", key="ib_backup", hel
 
 data['1.3. Примечание'] = st.text_area("Примечание к разделу 1.3", placeholder="Специфика серверного парка...", key="note_1_3")
 
+# 1.4 СХД
+st.write(---)
+st.subheader(1.4. Системы хранения данных (СХД))
+if st.toggle(Есть собственная СХД, key=storage_toggle)
+    st_media_sel = st.multiselect(Типы носителей, [HDD (NL-SAS  SATA), SSD (SATA  SAS), NVMe, SCM], key=st_media)
+    data['1.4.1. Типы носителей'] = st_media_sel
+    col_pct1, col_pct2 = st.columns(2)
+    with col_pct1
+        cnt_hdd = st.number_input(Количество дисков HDD, min_value=0, step=1, key=cnt_hdd)
+        data['1.4.2. Кол-во HDD'] = cnt_hdd
+    with col_pct2
+        cnt_ssd = st.number_input(Количество дисков SSD, min_value=0, step=1, key=cnt_ssd)
+        data['1.4.3. Кол-во SSD'] = cnt_ssd
+    
+    if st_media_sel and (cnt_hdd + cnt_ssd == 0)
+        st.info(ℹ️ Укажите количество дисков для СХД.)
+
+    col_chk1, col_chk2 = st.columns(2)
+    data['1.4.4. Гибридная СХД'] = col_chk1.checkbox(Используется гибридная СХД, key=hybrid_st)
+    data['1.4.5. All-Flash'] = col_chk2.checkbox(Есть All-Flash массивы, key=allflash_st)
+    data['1.4.6. RAID-группы'] = st.multiselect(Используемые RAID-группы, [RAID 0, RAID 1, RAID 5, RAID 6, RAID 10, RAID 50, RAID 60, JBOD], key=raid_list)
+
 # 1.5 Внутренние ИС
 st.write("---")
 st.subheader("1.5. Внутренние Информационные системы")
